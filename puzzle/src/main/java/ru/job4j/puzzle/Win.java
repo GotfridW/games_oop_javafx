f9fb0089be3abd9cd1337b49a7736bf491dd9bf8
Gotfrid <gotfridwallenstein@gmail.com>
2022-04-17 19:36:59 +0300

    Method "check" finalised, methods "checkRow" & "checkColumn" added.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,36 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
+        boolean rsl = false;
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][index] == 1
+                    && Win.checkRow(board, index) || Win.checkColumn(board, index)) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean checkRow(int[][] board, int row) {
         boolean rsl = true;
+        for (int index = 0; index < board.length; index++) {
+            if (board[row][index] != 1) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean checkColumn(int[][] board, int column) {
+        boolean rsl = true;
+        for (int[] index : board) {
+            if (index[column] != 1) {
+                rsl = false;
+                break;
+            }
+        }
         return rsl;
     }
 }
